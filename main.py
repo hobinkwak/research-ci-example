042f2199127b99f72d24873c1601b5d25522ef04
hobinkwak <khb0802@gmail.com>
2021-09-20 08:44:31 +0900

    unit test

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def                     hellow():
-    print("hobin")
+def hellow(a):
+    msg = f"hobin {a}"
+    print(msg)
+    return msg
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
         hellow()
